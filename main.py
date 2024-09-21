110dee34a6204c8cd96efe3961c5d4faf029e578
szcskgyr <szucsik.gyorgy@diak.szamalk-szalezi.hu>
2024-09-21 11:31:57 +0200

    update4

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import feladat7
-#import feladat8
-#import feladat9
-#import feladat10
-#import feladat11
-#import feladat12
-#import feladat13
-#import feladat14
-#import feladat15
-#import feladat16
-#import feladat17
-#import feladat18
-#import feladat19
-#import feladat20
-#import feladat21
+# import feladat8
+# import feladat9
+# import feladat10
+# import feladat11
+# import feladat12
+# import feladat13
+# import feladat14
+# import feladat15
+# import feladat16
+# import feladat17
+# import feladat18
+# import feladat19
+# import feladat20
+# import feladat21
